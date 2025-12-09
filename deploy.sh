5d898ea410d7bdc0d082d7e3ec3d3a28d8fd2f0f
Md Golam Rabbi <golamrabbi3@gmail.com>
2025-12-09 14:45:59 +0600

    removed sudo based command

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,6 @@ git pull --rebase
 
 composer install --no-dev --optimize-autoloader
 
-php artisan optimize:clear
-
 php artisan migrate --force
-
-php artisan config:cache
-
-# php artisan route:cache
-
-php artisan view:cache
 
 echo "Deploying completed"
