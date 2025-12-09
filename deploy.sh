9ce502b29b1527f1312a76251caafa43967ff078
Md Golam Rabbi <golamrabbi3@gmail.com>
2025-12-09 14:32:17 +0600

    added npm path

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,13 @@ set -e
 
 echo "Deploying..."
 
+# Load NVM if available
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Or add Node to PATH if installed globally
+export PATH="/usr/local/bin:/usr/bin:$PATH"
+
 git config --global --add safe.directory /var/www/equaldeals.com
 
 git pull --rebase
